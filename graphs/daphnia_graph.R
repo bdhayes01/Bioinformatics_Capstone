57146cac74fceebbc9f9ca7ff8ddf091e2f39347
bdhayes01 <105394669+bdhayes01@users.noreply.github.com>
2024-04-08 10:30:00 -0600

    Update daphnia_graph.R

--- graphs/daphnia_graph.R
+++ graphs/daphnia_graph.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-data = read.delim("https://raw.githubusercontent.com/bdhayes01/Bioinformatics_Capstone/main/Daphnia_population.txt")
+data = read.delim("https://raw.githubusercontent.com/bdhayes01/Bioinformatics_Capstone/main/data/Daphnia_population.txt")
 
 graph = ggplot(data = data, aes(x = 1:length(Population), y = Population)) +
   geom_point(alpha=0) +
